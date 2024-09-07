1e41242f069caf48b96d97917201bb3859149db8
taetae <dlaxodus1212@naver.com>
2024-09-07 17:39:58 +0900

    chapter_05

--- app-start/src/index.js
+++ app-start/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// 매초 최신 시간을 렌더링하는 방법
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+// CommentList 컴포넌트를 렌더링
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
