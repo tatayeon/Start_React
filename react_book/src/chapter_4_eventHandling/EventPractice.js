1c8ca3ae957224953390e490bcd687e8f0a1ad02
taetae <dlaxodus1212@naver.com>
2024-09-28 01:17:43 +0900

    임의의 메서드 만들기

--- react_book/src/chapter_4_eventHandling/EventPractice.js
+++ react_book/src/chapter_4_eventHandling/EventPractice.js
@@ -6,30 +6,38 @@ class EventPractice extends Component {
         message: ''
     }
 
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick(e){
+        this.setState({
+            message: ''
+        });
+    }
+
+
     render() {
         return(
             <dev>
                 <h1>이벤트 연습</h1>
                 <input type="text" name="message" placeholder="아무거나 입력해보세요"
-                onChange={(e) => {
-                    this.setState({
-                        message: e.target.value
-                    })
-                    }}>
+                value={this.state.message}
+                onChange={this.handleChange}
+                >
                 </input>
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }> 확인
-
-                </button>
+                <button onClick={this.handleClick}> 확인</button>
             </dev>
         )
     }
 }
-
+//임의의 메서드 만들기까지
 export default EventPractice;
