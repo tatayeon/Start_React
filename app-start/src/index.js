d16c65ec1a82aefc321f0d9c743835656f887506
taetae <dlaxodus1212@naver.com>
2024-09-27 23:47:31 +0900

    chapter_08 re

--- app-start/src/index.js
+++ app-start/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Accommodate from './chapter_07/Accommodate'
-
+import ConfirmButton from './chapter_08/ConfirmButton';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
