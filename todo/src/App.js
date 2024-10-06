8f61b6a634fcffbe8e1ea0feca486b7b159f66a2
taetae <dlaxodus1212@naver.com>
2024-10-06 20:29:34 +0900

    중간

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,48 @@
+import { useCallback, useRef, useState } from "react";
 import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate"
 
 const App = () => {
-  return (
-    <TodoTemplate>
-      <TodoInsert />
-    </TodoTemplate>
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: '리엑트 기초 알아보기',
+      checked:true,
+    },
+    {
+      id:2,
+      text: '컴포넌트 스타일링',
+      checked:true,
+    },
+    {
+      id:3,
+      text: '일정 관리앱 만들어보자',
+      checked:false
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.current(todo));
+      nextId.current += 1;
+    },
+    [todos],
   );
+
+  return(
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  )
 }
 
 export default App;
--- todo/src/components/TodoInsert.js
+++ todo/src/components/TodoInsert.js
@@ -1,15 +1,24 @@
 import {MdAdd} from 'react-icons/md'
+import {useState, useCallback} from 'react'
 import './TodoInsert.scss'
 
 const TodoInsert = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value)
+    }, [])
+
     return (
         <form className='TodoInsert'>
-            <input placeholder='할 일을 입력하세요' />
+            <input placeholder='할 일을 입력하세요'
+                    value={value}
+                    onChange={onChange}/>
                 <button type='submit'>
                     <MdAdd />
                 </button>
         </form>
-    )
-}
+    );
+};
 
 export default TodoInsert;
